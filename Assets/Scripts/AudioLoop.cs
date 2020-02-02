c3ddb8b9558f6cbd24a85ef738d599d167d5a4d9
Kai Salmon <kaisalmon@hotmail.com>
2020-02-02 14:49:02 +0000

    Music fixes

---
+++ Assets/Scripts/AudioLoop.cs
@@ -0,0 +1,23 @@
+﻿// When the audio component has stopped playing, play otherClip
+using UnityEngine;
+using System.Collections;
+
+public class AudioLoop : MonoBehaviour
+{
+    public AudioClip otherClip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = otherClip;
+            audioSource.Play();
+        }
+    }
+}
